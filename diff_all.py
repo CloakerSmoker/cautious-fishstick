51ba6b7a079a08a155cf3def4d69d1b54585c350
CloakerSmoker <43710474+CloakerSmoker@users.noreply.github.com>
2026-03-02 11:04:26 -0600

    scripts to diff against dats

---
+++ diff_all.py
@@ -0,0 +1,33 @@
+
+import sys, glob, os, shlex
+import xml.etree.ElementTree as ET
+
+shadow_dir = sys.argv[1]
+dats_dir = sys.argv[2]
+data_dir = sys.argv[3]
+
+missing = open('missing.txt', 'w')
+
+for dat in glob.glob('**/*.dat', root_dir=dats_dir, recursive=True):
+    dat_path = os.path.join(dats_dir, dat)
+    dat_info = ET.parse(dat_path).getroot()
+
+    dat_name = dat_info.find('.//header').find('name').text
+
+    dat_directory = os.path.dirname(dat) + '/' + dat_name
+    dat_shadow = os.path.join(shadow_dir, dat_directory)
+    dat_data = os.path.join(data_dir, dat_directory)
+
+    if os.path.exists(dat_shadow + '/present.txt'):
+        print(f'{dat_shadow} already exists, skipping')
+
+    if not os.path.exists(os.path.join(data_dir, dat_directory)):
+        print(f'Directory {dat_directory} not found, skipping')
+        missing.write(f'{dat_directory}\n')
+    else:
+        print(f'Comparing {dat_directory}')
+        #print(f'python diff_dat.py {shlex.quote(dat_shadow)} {shlex.quote(dat_data + "/")} {shlex.quote(dat_path)}')
+        #break
+        os.system(f'python diff_dat.py {shlex.quote(dat_shadow)} {shlex.quote(dat_data + "/")} {shlex.quote(dat_path)}')
+
+
---
+++ diff_dat.py
@@ -0,0 +1,123 @@
+
+import sys, os, hashlib, zipfile, pathlib
+import xml.etree.ElementTree as ET
+import tqdm
+
+shadow_dir = sys.argv[1]
+base_dir = sys.argv[2]
+
+xml_file = sys.argv[3]
+
+tree = ET.parse(xml_file)
+
+root = tree.getroot()
+games = root.findall('.//game')
+
+class FileInfo:
+    def __init__(self, name, size, crc):
+        self.name = name
+        self.size = size
+        self.crc = crc
+    
+    def __eq__(self, value):
+        return self.name == value.name and self.size == value.size and self.crc == value.crc
+
+class ArchiveInfo:
+    def __init__(self, name, children):
+        self.name = name
+        self.children = children
+    
+    def __eq__(self, value):
+        if len(self.children) != len(value.children):
+            return False
+
+        for crc in self.children:
+            if not crc in value.children:
+                return False
+        
+        return True
+
+existing_files = {}
+
+index = 0
+
+for file_name in tqdm.tqdm(os.listdir(base_dir)):
+    zf = zipfile.ZipFile(base_dir + file_name)
+    
+    children = {}
+
+    for ze in zf.infolist():
+        children[ze.CRC] = FileInfo(ze.filename, ze.file_size, ze.CRC)
+
+    name = file_name.replace('.zip', '')
+    existing_files[name] = ArchiveInfo(name, children)
+
+    index += 1
+
+    #if index > 30:
+    #    break
+
+dat_files = {}
+
+for game in tqdm.tqdm(games):
+    roms = game.findall('rom')
+
+    children = {}
+
+    for rom in roms:
+        name = rom.get('name')
+        size = int(rom.get('size'))
+        crc = int(rom.get('crc'), 16)
+
+        children[crc] = FileInfo(name, size, crc)
+
+    name = game.get('name')
+    dat_files[name] = ArchiveInfo(name, children)
+
+present = []
+missing = []
+bad = []
+deleted = []
+renamed = {}
+
+for name, info in dat_files.items():
+    existing_info = existing_files.get(name, None)
+
+    if not existing_info:
+        # Check if there's a file with the same content but different name
+
+        matching_identity_infos = [existing_info for existing_info in existing_files.values() if existing_info == info]
+
+        if len(matching_identity_infos):
+            # name not found, identity found
+
+            print('R', info.name)
+            renamed[matching_identity_infos[0].name] = info.name
+        else:
+            # name not found, identity not found
+
+            print('M', info.name)
+            missing.append(info)
+    else:
+        if existing_info != info:
+            # name found, identity doesn't match
+
+            print('B', info.name)
+            bad.append(info)
+        else:
+            # name found, identity matches
+
+            print('P', info.name)
+            present.append(info)
+
+for name, info in existing_files.items():
+    if name not in dat_files and name not in renamed:
+        print('D', info.name)
+        deleted.append(info.name)
+
+os.makedirs(shadow_dir, exist_ok=True)
+open(shadow_dir + '/present.txt', 'w').write('\n'.join([f"{f.name}" for f in present]))
+open(shadow_dir + '/missing.txt', 'w').write('\n'.join([f"{f.name}" for f in missing]))
+open(shadow_dir + '/bad.txt', 'w').write('\n'.join([f"{f.name}" for f in bad]))
+open(shadow_dir + '/deleted.txt', 'w').write('\n'.join(deleted))
+open(shadow_dir + '/renamed.txt', 'w').write('\n'.join([f"{old} -> {new}" for old, new in renamed.items()]))
